ee486698554cf8834b9da31d299f4ac3fabc7627
bard <bartoszslesar@gmail.com>
2023-10-26 15:36:52 +0200

    project initialization

---
+++ src/test/java/com/bard/kursspring/KursSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bard.kursspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
